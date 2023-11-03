4d01746c40e87502b34d84bed425a9e34d9a3cb2
Uexcel2022 <excellenceudoka@gmail.com>
2023-11-03 16:18:06 +0100

    commit

--- src/main/java/com/uexcel/spring/security/event/ApplicationEventList.java
+++ src/main/java/com/uexcel/spring/security/event/ApplicationEventList.java
@@ -1,14 +1,10 @@
 package com.uexcel.spring.security.event;
 
 import com.uexcel.spring.security.entity.User;
-import com.uexcel.spring.security.entity.UserToken;
-import com.uexcel.spring.security.repository.UserTokenRepository;
 import com.uexcel.spring.security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 
-import java.util.Calendar;
-import java.util.Date;
 import java.util.UUID;
 
 public class ApplicationEventList implements
